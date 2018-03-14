4f91203bf75588cdb1f4affa897be6d7e52a4bec
NianLiv <au564992@uni.au.dk>
2018-03-14 12:29:55 +0100

    NewDepitorWindow oprettet.

--- DeptBook/NewDepitor.xaml.cs
+++ DeptBook/NewDepitor.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace DeptBook
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for NewDepitor.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class NewDepitor : Window
     {
-        public MainWindow()
+        public NewDepitor()
         {
             InitializeComponent();
-        }
-
-        private void btnDebitor_Click(object sender, RoutedEventArgs e)
-        {
-            var newDepitorWin = new NewDepitor();
-            newDepitorWin.WindowStartupLocation = WindowStartupLocation.CenterScreen;
-            newDepitorWin.Show();
         }
     }
 }
