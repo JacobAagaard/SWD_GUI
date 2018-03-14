ff29a12473c7e42b9137dc291e194b96b4e37a0f
NianLiv <au564992@uni.au.dk>
2018-03-14 12:38:43 +0100

    Er du model?

--- DeptBook/Model/DBModel.cs
+++ DeptBook/Model/DBModel.cs
@@ -6,8 +6,62 @@ using System.Threading.Tasks;
 
 namespace DeptBook.Model
 {
-    class DBModel
+    class Debitor
     {
+        public string Name { get; set; }
+        private static int _idCount = 0;
+        private int _id;
 
+        public Debitor(string name)
+        {
+            Name = name;
+            _id = ++_idCount;
+        }
+
+        public int GetID()
+        {
+            return _id;
+        }
+    }
+
+    class DeptBook
+    {
+        private Dictionary<int, List<Transaction>> transactions;
+
+        public DeptBook()
+        {
+            transactions = new Dictionary<int, List<Transaction>>();
+        }
+
+        public void AddDebitor(Debitor debitor)
+        {
+            transactions.Add(debitor.GetID(), new List<Transaction>());
+        }
+
+        public void NewTransaction(int ID, Transaction transaction)
+        {
+            var transactionList = new List<Transaction>();
+            transactions.TryGetValue(ID, out transactionList);
+            transactionList.Add(transaction);
+        }
+
+        public int GetBalanceByID(int id)
+        {
+            int amount = 0;
+            List<Transaction> transactionList;
+            transactions.TryGetValue(id, out transactionList);
+            foreach (var transaction in transactionList)
+            {
+                amount += transaction.Amount;
+            }
+
+            return amount;
+        }
+    }
+
+    class Transaction
+    {
+        public int Amount { get; set; }
+        public string Description { get; set; }
     }
 }
